4f613ce679a121b689a5b02fea7f8585ec53b129
Yuji Tani <tani@leanonme.co.jp>
2022-07-24 04:28:30 +0900

    ルーティングを変更できるように試行錯誤してみた

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express(); 
 const PORT = 5000;
-const usersFunc = require("./Data/users.js");
-const users = usersFunc() ;
+const repositories = require('./api/v1/index');
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
+app.get("/api/v1/users", (req, res) => {
     res.send(usersFunc());
 })
 
-app.post("/users", (req, res) => {
+// ユーザー登録
+app.post("/api/v1/users", (req, res) => {
     const user = {
         id: users.length + 1,
         name: req.body.name,
@@ -23,16 +23,26 @@ app.post("/users", (req, res) => {
 })
 
 // ユーザー情報更新
-app.put("/users/:id", (req, res) => {
+app.put("/api/v1/users/:id", (req, res) => {
     const user = users.find((user) => {
         return user.id === parseInt(req.params.id)
     })
 
-    console.log(user);
-
     user.name = req.body.name
     res.send(user);
 })
+
+// ユーザー削除
+app.delete("/api/v1/users/:id", (req, res) => {
+    users = users.filter((user) => {
+        return user.id !== parseInt(req.params.id)
+    })
+
+    console.log(users);
+
+    res.send(`ユーザーID ${req.params.id}を削除しました。`);
+})
+
 
 app.listen(PORT, () => {
     console.log('http://localhost:5000/');
