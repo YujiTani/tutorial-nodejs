2f07b88e9b8a0c590f0ef36e93c5dad7244790ca
Yuji Tani <tani@leanonme.co.jp>
2022-07-24 01:33:56 +0900

    HTTPメソッドを叩く処理を追加

---
+++ Data/users.js
@@ -0,0 +1,42 @@
+module.exports = function users() {
+    const users = [
+        {
+            id: 1,
+            name: "山田 太郎",
+            age: 18,
+            gender: "Male"
+        },
+        {
+            id: 2,
+            name: "山田 花子",
+            age: 18,
+            gender: "Female"
+        },
+        {
+            id: 3,
+            name: "佐藤 健",
+            age: 29,
+            gender: "Male"
+        },
+        {
+            id: 4,
+            name: "小川 太一",
+            age: 23,
+            gender: "Male"
+        },
+        {
+            id: 5,
+            name: "菊池 あかり",
+            age: 15,
+            gender: "Female"
+        },
+        {
+            id: 6,
+            name: "天道 蒼太",
+            age: 18,
+            gender: "Male"
+        }
+    ]
+
+    return users;
+};
--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const app = express(); 
-const PORT = 8888;
+const PORT = 5000;
+const usersFunc = require("./Data/users.js");
+const users = usersFunc() ;
+
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+    res.send(usersFunc());
+})
+
+app.post("/users", (req, res) => {
+    const user = {
+        id: users.length + 1,
+        name: req.body.name,
+        age: req.body.age,
+        gender: req.body.gender
+    }
+    
+    users.push(user);
+    res.send(user);
+})
+
+// ユーザー情報更新
+app.put("/users/:id", (req, res) => {
+    const user = users.find((user) => {
+        return user.id === parseInt(req.params.id)
+    })
+
+    console.log(user);
+
+    user.name = req.body.name
+    res.send(user);
+})
 
 app.listen(PORT, () => {
-    console.log('server running!!');
+    console.log('http://localhost:5000/');
 })
