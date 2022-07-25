d243a0cda6fc5441235512755a9af1a6b2f68d8d
Yuji Tani <tani@leanonme.co.jp>
2022-07-26 06:41:34 +0900

    mongoDBと接続し、データのやり取りができるようにした

--- src/api/v1/Controllers/tasks.js
+++ src/api/v1/Controllers/tasks.js
@@ -1,27 +1,76 @@
-const getAll = (req, res) => {
-    res.send('タスク一覧を取得しました。')   
-}
+const Task = require("../Model/Task");
 
-const get = (req, res) => {
-    res.send('タスクを取得しました。')
-}
+const getAll = async (req, res) => {
+  try {
+    const allTask = await Task.find({});
+    res.status(200).json(allTask);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-const create = (req, res) => {
-    res.send('タスクを新規作成しました。')
-}
+const get = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findById(id).exec();
+    if (!task) {
+      res.status(404).json(`ID:${id}は存在しません`);
+    }
 
-const update = (req, res) => {
-    res.send('タスクを更新しました。')
-}
+    res.status(200).json(task);
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(404).json(`不正なIDを検出しました`);
+    }
+    res.status(500).json(error);
+  }
+};
 
-const deleteSingle = (req, res) => {
-    res.send('タスクを削除しました。')
-}
+const create = async (req, res) => {
+  try {
+    const createTask = await Task.create(req.body);
+    res.status(200).json(createTask);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateTask = await Task.findByIdAndUpdate(id, req.body);
+
+    if (!updateTask) {
+      res.status(404).json(`ID:${id}は存在しません`);
+    }
+
+    res.status(200).json(updateTask);
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(404).json(`不正なIDを検出しました`);
+    }
+    res.status(500).json(error);
+  }
+};
+
+const deleteSingle = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Task.findByIdAndDelete(id);
+
+    res.status(200).json(`ID:${id}は正常に削除されました`);
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(404).json(`不正なIDを検出しました`);
+    }
+    res.status(500).json(error);
+  }
+};
 
 module.exports = {
-    getAll,
-    get,
-    create,
-    update,
-    deleteSingle,
-}
+  getAll,
+  get,
+  create,
+  update,
+  deleteSingle,
+};
--- src/app.js
+++ src/app.js
@@ -3,13 +3,27 @@ const app = express();
 const PORT = 5000;
 const userRoute = require('./api/v1/Routes/users');
 const taskRoute = require('./api/v1/Routes/tasks');
+const connectDB = require("./api/v1/DB/connect");
+require('dotenv').config();
 
 app.use(express.json());
 
+// ルーティング
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/tasks", taskRoute);
 
+const start = async() => {
+    try {
+        // DB接続
+        connectDB(process.env.MONGO_URL)
 
-app.listen(PORT, () => {
-    console.log('http://localhost:5000/');
-})
+        app.listen(PORT, () => {
+            console.log('start server');
+            console.log('http://localhost:5000/');
+        })
+    } catch (error) {
+        console.warn(error)
+    }
+}
+
+start()
